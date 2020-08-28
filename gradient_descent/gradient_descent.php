503acce161a9bb084b0d285f4e6910c3355e4d33
jonjlake <jonjlake@gmail.com>
2020-08-28 02:30:56 -0500

    Adding csv file writing each iteration for graphing and debugging

--- gradient_descent/gradient_descent.php
+++ gradient_descent/gradient_descent.php
@@ -1,6 +1,20 @@
 <?php
   run_tests();
 
+  function print_static_header($fh)
+  {
+    fwrite($fh, "alpha, max_iterations, pct_diff_conv, ") or die("Unable to print static header");
+  }
+  function print_csv_header($fh)
+  {
+    fwrite($fh, "Iteration, theta0, theta1, cost_fn, cost_pct_diff\n") or die("Unable to write header to csv file"); 
+  }
+
+  function print_iteration_values($fh, $col_padding, $iteration, $theta0, $theta1, $cost, $cost_pct_diff)
+  {
+    fwrite($fh, $col_padding." ".$iteration.",".$theta0.",".$theta1.",".$cost.",".$cost_pct_diff."\n") or die("Unable to write iteration values to file");
+  } 
+
   function hypothesis($theta0, $theta1, $x)
   {
     return $theta0 + $theta1 * $x;
@@ -84,13 +98,27 @@
 
   function gradient_descent($x_vals, $y_vals, $alpha)
   {
+    $debug_filename = "./files/gradient_descent.csv";
+    $fh = fopen($debug_filename, 'w') or die("Failed to create gradient descent debug file");
     $theta0_i = 0;
     $theta1_i = 0;
     $theta0_new = 0;
     $theta1_new = 0;
-    $max_iterations = 1000;
+    $max_iterations = 10000;
 //    $max_iterations = 4;
-    $pct_diff_conv = 0.01;
+//    $pct_diff_conv = 0.01;
+//    $pct_diff_conv = 0.001;
+    $pct_diff_conv = 0.0001;
+
+    print_static_header($fh);
+    print_csv_header($fh);
+
+    fwrite($fh, $alpha.",".$max_iterations.",".$pct_diff_conv.",");
+
+    $cost = cost($theta0_i, $theta1_i, $x_vals, $y_vals);
+    $j = 0;
+    print_iteration_values($fh, "", $j, $theta0_new, $theta1_new, $cost, 0);
+    
     for ($j = 0; $j < $max_iterations; ++$j)
     {
       $theta0_new = $theta0_i - $alpha * dcost_dtheta0($theta0_i, $theta1_i, $x_vals, $y_vals);
@@ -115,10 +143,15 @@
       {
         echo "Failed to converge in $max_iterations iterations<br>";
       }
+
+      // check iteration number, and whether this will print the 1st value
+      print_iteration_values($fh, ",,,", $j, $theta0_new, $theta1_new, $cost, 0);
       $theta0_i = $theta0_new;
       $theta1_i = $theta1_new; 
     } 
     $thetas = array($theta0_new, $theta1_new);
+
+    fclose($fh);
     return $thetas;
   }
 
@@ -126,7 +159,7 @@
   {
     $x_vals = array(1, 2, 3);
     $y_vals = array(2.5, 7.5, 11);
-    $alpha = 0.001;
+    $alpha = 0.0001;
     $theta_exp = array(0, 0);
     $theta_act = gradient_descent($x_vals, $y_vals, $alpha);
     report_test_results("gradient_descent_theta0", $theta_act[0], $theta_exp[0]);
